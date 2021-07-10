c795cf72e71a7c15dbbc2589adf881a7a266ae59
caiomcs04 <caio6c@yahoo.com.br>
2021-07-10 20:27:50 -0300

    5#commit(estilo e implementação do ngIf)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NewOperationComponent } from './new-operation/new-operation.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExtractComponent } from './extract/extract.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+/*Esse methodo do angular serve para fazer a localização da data.
+necessário importar tanto o metodo quando a variavel localePt*/
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   /*Aqui deve ser declarado todos os componentes que serão usados*/
@@ -17,7 +23,15 @@ import { ExtractComponent } from './extract/extract.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    /*Aqui em providers será declarada as localizações tanto de data
+    quanto de valor monetario para nosso aplicativo*/
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
